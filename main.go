bb4bf1db70a8f0368e08143015943d07a80e275a
Robin B <robin.buergi@gmail.com>
2017-03-29 21:11:39 +0200

    refactoring

--- main.go
+++ main.go
@@ -38,40 +38,57 @@ func main() {
 		os.Exit(1)
 	}
 
-	response, err := http.Get("http://transport.opendata.ch/v1/connections?from=" + os.Args[1] + "&to=" + os.Args[2] + "&fields[]=from/name&fields[]=to/name&fields[]=connections/from/platform&fields[]=connections/from/departureTimestamp&fields[]=connections/to/arrivalTimestamp")
+	response := makeRequest(os.Args[1], os.Args[2]) // TODO validation
+
+	fmt.Printf("\n%v to %v\n\n", strings.ToUpper(response.From.Name), strings.ToUpper(response.To.Name))
+
+	tableData := buildTableData(response)
+	tableHeader := []string{"Nr", "Platform", "Departure Time", "Arrival Time"}
+
+	writeTable(tableHeader, tableData)
+	println()
+}
+
+func buildTableData(response responseStruct) [][]string {
+	var tableData [][]string
+	for i, con := range response.Connections {
+		tableData = append(tableData, []string{
+			strconv.Itoa(i),
+			con.From.Platform,
+			time.Unix(int64(con.From.DepartureTimestamp), 0).Format("15:04:05"),
+			time.Unix(int64(con.To.ArrivalTimestamp), 0).Format("15:04:05")})
+	}
+
+	return tableData
+}
+
+func makeRequest(from string, to string) responseStruct {
+	getResponse, err := http.Get("http://transport.opendata.ch/v1/connections?from=" + from + "&to=" + to + "&fields[]=from/name&fields[]=to/name&fields[]=connections/from/platform&fields[]=connections/from/departureTimestamp&fields[]=connections/to/arrivalTimestamp")
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		body, err := ioutil.ReadAll(response.Body)
-		defer response.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var response responseStruct
-		err = json.Unmarshal(body, &response)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("\n%v to %v\n\n", strings.ToUpper(response.From.Name), strings.ToUpper(response.To.Name))
-
-		var data [][]string
-
-		for i, con := range response.Connections {
-			data = append(data, []string{
-				strconv.Itoa(i),
-				con.From.Platform,
-				time.Unix(int64(con.From.DepartureTimestamp), 0).Format("15:04:05"),
-				time.Unix(int64(con.To.ArrivalTimestamp), 0).Format("15:04:05")})
-		}
-
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Nr", "Platform", "Departure Time", "Arrival Time"})
-
-		for _, v := range data {
-			table.Append(v)
-		}
-		table.Render() // Send output
 	}
+
+	body, err := ioutil.ReadAll(getResponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer getResponse.Body.Close()
+
+	var response responseStruct
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return response
+}
+
+func writeTable(header []string, data [][]string) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(header)
+
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.Render()
 }
